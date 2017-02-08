f505b08dd8d8aeefc79c41353239e2d94e199951
Kacper Opyrchał <opyrchalkacper@gmail.com>
2017-02-09 00:46:00 +0100

    1) Test

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -4,26 +4,53 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g3d.particles.influencers.ColorInfluencer;
-import com.badlogic.gdx.graphics.g3d.particles.influencers.ParticleControllerFinalizerInfluencer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
 import java.util.Random;
-
-import sun.rmi.runtime.Log;
 
 public class
 MyGdxGame extends ApplicationAdapter implements InputProcessor{
 	ShapeRenderer shapeRenderer;
 	Batch batch;
 
+	Texture img;
+	TiledMap tiledMap;
+	OrthographicCamera camera;
+	TiledMapRenderer tiledMapRenderer;
+
+
+	public MyGdxGame(){
+		super();
+
+
+	}
+
+
 	@Override
 	public void create() {
 
+		float w = Gdx.graphics.getWidth();
+		float h = Gdx.graphics.getHeight();
+
 		shapeRenderer = new ShapeRenderer();
 		batch = new SpriteBatch();
+
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false,w,h);
+		camera.update();
+		tiledMap = new TmxMapLoader().load("test.tmx");
+
+		tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
+
 
 		RenderBoard.init();
 		Gdx.input.setInputProcessor(this);
@@ -32,8 +59,17 @@ MyGdxGame extends ApplicationAdapter implements InputProcessor{
 	public void resize(int width, int height) {}
 	@Override
 	public void render() {
-		Gdx.gl.glClearColor( 0, 0, 1, 1);
+		Gdx.gl.glClearColor( 1, 1, 1, 1);
+		Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		camera.update();
+		tiledMapRenderer.setView(camera);
+		tiledMapRenderer.render();
+
+		camera.translate(1,1);
+
+		RenderBoard.renderGrid(shapeRenderer, 18, 32);
 
 	}
 
@@ -64,6 +100,9 @@ MyGdxGame extends ApplicationAdapter implements InputProcessor{
 
 	@Override
 	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		Random rand = new Random();
+
+		camera.translate(-100,-100);
 
 		return true;
 	}
--- core/src/com/mygdx/game/RenderBoard.java
+++ core/src/com/mygdx/game/RenderBoard.java
@@ -19,5 +19,25 @@ public class RenderBoard {
         font = new BitmapFont();
     }
 
+    public static void renderGrid(ShapeRenderer shapeRenderer, int xSize, int ySize){
+        int x = Gdx.graphics.getWidth();
+        int y = Gdx.graphics.getHeight();
+
+        int nx = xSize;
+        int ny = ySize;
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        shapeRenderer.setColor(1, 1, 0.30f, 0);
+
+        for(int i = 0; i <= ny; ++i){
+            shapeRenderer.rectLine(0, i*(y/ny), x, i*(y/ny), 1);
+        }
+        for(int i = 0; i <= nx; ++i){
+            shapeRenderer.rectLine(i*(x/nx), 0, i*(x/nx), y, 1);
+        }
+
+        shapeRenderer.end();
+
+    }
+
 
 }
