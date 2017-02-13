5958d44f08a93bda3d0749ad30f73256505df064
Kacper Opyrchał <opyrchalkacper@gmail.com>
2017-02-13 16:51:00 +0100

    1) Show error

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -49,7 +49,7 @@ MyGdxGame extends ApplicationAdapter implements InputProcessor{
 	@Override
 	public void create() {
 
-		board = new Board(20, 30);
+		board = new Board(6, 12);
 
 		testCell = new Cell(100, 100, 100, Color.GREEN);
 		//testCell.setTextureHexagon(Color.GREEN);
